8b347cbe8b4cf3892d02759e8003b641fb5c9200
pefeijoo <pablo.estevez.feijoo@gmail.com>
2018-11-27 18:48:28 +0100

    modifique el showall del campeonato

--- Views/Campeonato/Campeonato_SHOWALL.php
+++ Views/Campeonato/Campeonato_SHOWALL.php
@@ -1,97 +1,25 @@
 <?php
 /* 
-	Vista para mostrar todos los campeonatos
+	Vista para mostrar todas los campeonatos
 */
 include '../Views/base/SHOWALL.php';
-	
+
 class Campeonato_SHOWALL extends SHOWALL{  // declaracion de clase
 	// declaracion constructor de la clase
 	// se inicializa con los valores del formulario y el valor del botÃ³n submit pulsado
+	var $showADDCATEGORIA = true;
 	
-	var $ADDCATEGORIA = true;
-
 	function __construct($respuesta){
-		$this->controlador = 'controller_Campeonato.php';
+		$this->controlador = 'Controller_Campeonato.php';
 		$this->resultado = $respuesta;
 		$this->toString();
 	} // fin del constructor
-
-function toString(){
-		include '../Views/base/header.php';
-		/**COMIENZO TABLA**/
-		echo '<table class="tabla">';
-			echo "<form id='AnadirBuscar' method='GET' action='"; echo $this->controlador; echo "'>";
-			echo '<tr class="filaInvisible">';
-				echo '<td class="celdaInvisible">';
-				echo "<input class='submit' type='submit' name='submit' value='ADD'/>";
-				echo '</td>';
-				
-				echo '<td class="celdaInvisible">';
-				echo "<input class='submit' type='submit' name='submit' value='SEARCH'/>";
-				echo '</td>';
-			echo '</tr>';
-			echo '</form>';
-			/**COMIENZO FILA TITULOS COLUMNA**/
-			echo '<tr class="fila">';
-				/*Para cada field (campo) se muestra su nombre*/
-				while($tituloColumna = $this->resultado->fetch_field()){
-					echo '<th class="tituloColumna">';
-						echo $tituloColumna->name;
-					echo '</th>';
-				}
-				echo '<th class="tituloColumna">';
-				echo $this->acciones;
-				echo '</th>';
-				/*Columna de acciones*/
-			echo '</tr>';
-			/**FIN FILA TITULOS COLUMNA**/
-			
-			/**COMIENZO FILAS CON DATOS**/
-			/*Para cada fila se muestran los datos recuperados*/
-			$num = 0;
-			while($fila = $this->resultado->fetch_row()){
-				/*Se crea un formulario para dicha fila*/
-				echo "<form id='formularioOpcion"; echo $num; echo "' method='GET' action='"; echo $this->controlador; echo "'>";
-				/*Se crea una fila <tr>*/
-				echo '<tr class="fila">';
-				
-				/*Se crean tantas celdas y se muestran sus datos como tenga la fila*/
-				for($i = 0; $i < sizeof($fila); $i++){
-					echo '<td class="celda">';
-					echo "<input type='hidden' name='"; echo $this->resultado->fetch_fields()[$i]->name; echo "' value=\""; echo $fila[$i]; echo "\">";
-					echo $fila[$i];
-					echo '</td>';
-				}
-				
-				/*Se crean los botones para las acciones*/
-				//TODO: Poner los submit hidden y hacer un botón que haga submit de los formularios, esto es para decorar
-					echo '<td class="celda">';
-					if($this->EDIT){
-						echo "<input class='submit' type='submit' name='submit' value='EDIT'/>";
-					}
-					if($this->DELETE){
-						echo "<input class='submit' type='submit' name='submit' value='DELETE'/>";
-					}
-					if($this->SHOWCURRENT){
-						echo "<input class='submit' type='submit' name='submit' value='SHOWCURRENT'/>";
-					}
-					if($this->ADDCATEGORIA){
-						echo "<input class='submit' type='submit' name='submit' value='ADDCATEGORIA'/>";
-					}
-					echo '</td>';
-					
-				echo '</tr>';
-				echo '</form>';
-				$num++;
-			}
-			/**FIN FILAS CON DATOS**/
-			
-		/**FIN TABLA**/
-		echo '</table>';
-		include '../Views/base/footer.php';
+	
+	
+	function botonesOpcion(){
+		if($this->showADDCATEGORIA){
+			echo "<input class='btn btn-secondary' type='submit' name='submit' value='ADDCATEGORIA'/>";
+		}
 	}
-
-
-
 } 
-?>
+ ?>
